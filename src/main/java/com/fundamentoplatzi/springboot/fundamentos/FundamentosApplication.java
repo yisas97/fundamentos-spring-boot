fba4e869b06c5149c06e2720ffd24a5d069a3cfa
jesus <jjcamposv@uni.pe>
2021-12-19 06:21:53 -0500

    Agregar los archivos de spring

---
+++ src/main/java/com/fundamentoplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,14 @@
+package com.fundamentoplatzi.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication
+{
+	public static void main(String[] args)
+	{
+		//el metodo main
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+}
